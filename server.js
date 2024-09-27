752d88dc31adce239528e6a6633de7468f23e5cb
Yashvardhan <vs21yash@gmail.com>
2024-09-27 13:49:17 +0530

    cors ;-;

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors'); // Import cors
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors"); // Import cors
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-io.on('connection', (socket) => {
-    console.log('New client connected');
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
-app.post('/send-alert', (req, res) => {
-    const { message } = req.body;
-    io.emit('alert', message);
-    res.status(200).send('Alert sent!');
+app.post("/send-alert", (req, res) => {
+  const { message } = req.body.message;
+  io.emit("broadcastMessage", message);
+  res.status(200).send("Alert sent!");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log('Server running on port ' + PORT));
